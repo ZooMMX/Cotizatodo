29ba4e76bf870fa1e709b0a6cc967cddcfd788f4
Octavio Ruiz <octavio@servicios-corporativos.com>
2015-04-01 13:29:16 -0600

    Internacionalización

--- src/main/java/com/phesus/cotizatodo/MvcConfig.java
+++ src/main/java/com/phesus/cotizatodo/MvcConfig.java
@@ -1,8 +1,17 @@
 package com.phesus.cotizatodo;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 /**
  * Proyecto Omoikane: SmartPOS 2.0
@@ -22,6 +31,45 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         //registry.addViewController("/403").setViewName("403");
         //registry.addViewController("/404").setViewName("404");
         //registry.addViewController("/error").setViewName("error");
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource result
+         = new ResourceBundleMessageSource();
+
+        String[] basenames = {
+         "i18n.messages"
+        };
+
+        result.setBasenames(basenames);
+
+        return result;
+
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+
+        LocaleChangeInterceptor result = new LocaleChangeInterceptor();
+        result.setParamName("lang");
+
+        return result;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+
+        SessionLocaleResolver result = new SessionLocaleResolver();
+        result.setDefaultLocale(Locale.ENGLISH);
+
+        return result;
+
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+     registry.addInterceptor(localeChangeInterceptor());
     }
 
 }
